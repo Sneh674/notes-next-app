556ac8c339b2168123b38e93808ed357eed42f04
Sneh674 <naiksneh6@gmail.com>
2025-03-20 19:49:55 +0530

    see more page created

---
+++ src/app/client/notes/[id]/seemore/page.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import { useState, useEffect, } from "react";
+import axios from "axios";
+import { useRouter } from "next/navigation";
+import { useParams } from "next/navigation";
+
+export default function seeMore(){
+    return (
+        <div>
+            <h1>Seemore</h1>
+        </div>
+    )
+}
