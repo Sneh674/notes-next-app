5cac5033fe4026f64259c1374cc14d2fcc5f9cac
Sneh674 <naiksneh6@gmail.com>
2025-03-25 20:16:18 +0530

    commented see more psge

--- src/app/client/notes/[id]/seemore/page.tsx
+++ src/app/client/notes/[id]/seemore/page.tsx
@@ -1,14 +1,14 @@
-"use client";
+// "use client";
 
-import { useState, useEffect, } from "react";
-import axios from "axios";
-import { useRouter } from "next/navigation";
-import { useParams } from "next/navigation";
+// import { useState, useEffect, } from "react";
+// import axios from "axios";
+// import { useRouter } from "next/navigation";
+// import { useParams } from "next/navigation";
 
-export default function seeMore(){
-    return (
-        <div>
-            <h1>Seemore</h1>
-        </div>
-    )
-}
+// export default function seeMore(){
+//     return (
+//         <div>
+//             <h1>Seemore</h1>
+//         </div>
+//     )
+// }
